728d8812c8566893e6f38924dd159d0f2328bd66
kareem0018 <43916098+kareem0018@users.noreply.github.com>
2018-11-04 14:41:50 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,277 @@
+const Discord = require('discord.js');
+const bot1 = new Discord.Client();
+
+
+bot1.on('ready', () => {
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${bot1.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${bot1.guilds.size} " ]`);
+  console.log(`Users! [ " ${bot1.users.size} " ]`);
+  console.log(`channels! [ " ${bot1.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+  console.log('')
+});
+ 
+
+ bot1.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+
+ bot1.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+
+bot1.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('kareem is my dad')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+bot1.login("NTA4MzQ1NjYyMzU4NjgzNjU5.DsBt6g.kKRqwOcIYuPXn2HNXvo0wjb01hc")
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+const bot2 = new Discord.Client();
+ bot2.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot2.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot2.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('kef 7alk ya spam??')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+bot2.login("NTA4MzQ3NjEzNDE0Njg2NzIy.DsBt8g.qDS96D9OVDvGGFtDb1QJvF9r9SM")
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+
+const bot3 = new Discord.Client();
+ bot3.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot3.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot3.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('i love credits')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+
+bot3.login("NTA4MzQ4MzI2MDAxOTAxNTc4.DsBt_A.CkQK6mMLOpVcCwaNCo_X74HWsP4")
+
+////////////////////////////////////////////////////////////////////////////////
+
+const bot4 = new Discord.Client();
+ bot4.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot4.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot4.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('spam jmel')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+bot4.login("NTA4MzQ4OTk0ODcxODg1ODM1.DsBuLw.Ue_GvPSuIySeSr98vMSHDLii6ts")
+
+////////////////////////////////////////////////////////////////////////////////
+
+const bot5 = new Discord.Client();
+ bot5.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot5.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot5.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('wasseem bad')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+bot5.login("NTA4MzQ5OTEzMzE1MTQ3Nzc2.DsBuTA.qfobUoJlJDBwWQi8Kv9xl8YvjyI")
+
+///////////////////////////////////////////////////////////////////////////////////
+const bot6 = new Discord.Client();
+ bot6.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot6.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot6.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('i love kareem')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+bot6.login("NTA4MzUwNDAwMzk5NjcxMjk2.DsBubw.YXjROLRqIwu5jfui0wYlR94yhgU")
+//////////////////////////////////////////////////////////////////////
+const bot7 = new Discord.Client();
+ bot7.on('message', message => {
+  
+  if (message.content === 'Do your Daily') {
+
+message.channel.send('#daily')
+}
+});
+
+ bot7.on('message', message => {
+  
+  if (message.content === 'do rep') {
+
+message.channel.send('#rep <@474573718967025665>')
+}
+});
+
+bot7.on('message', message => {
+if (message.content === '!spam') {
+      let count = 0;
+      let ecount = 0;
+      for(let x = 0; x < 90000; x++) {
+        message.channel.send('i love spam')
+          .then(m => {
+            count++;
+          })
+
+        }
+      }
+});
+
+
+bot7.login("NTA4MzQxNjAxNTg5MDAyMjUw.DsBugw.DPNsWfM1qssIlkoaaMwPyClgtJ8")
+////////////////////////////////////////////////////////////////////////////
