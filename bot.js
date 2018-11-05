c85e33d4915254482f456e616d3ffe18e5447a85
eoijfoiwejfoijwfeoijfeoifwej <43916098+oiejoijweojoweijowej@users.noreply.github.com>
2018-11-05 21:53:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -74,8 +74,8 @@ message.channel.send(args.join("  "))
 
 
 
-bot1.login("NTA4MzQ1NjYyMzU4NjgzNjU5.DsBt6g.kKRqwOcIYuPXn2HNXvo0wjb01hc
-")
+bot1.login('NTA4MzQ1NjYyMzU4NjgzNjU5.DsBt6g.kKRqwOcIYuPXn2HNXvo0wjb01hc
+')
 
 
 
